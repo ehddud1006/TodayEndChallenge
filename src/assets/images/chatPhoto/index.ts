8abd550a0d94727811d38b271f07f51e63f22d13
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-04-08 13:35:51 +0900

    update

--- src/assets/images/chatPhoto/index.ts
+++ src/assets/images/chatPhoto/index.ts
@@ -3,5 +3,11 @@ import p1 from './p1.jpg';
 import p2 from './p2.jpg';
 import p3 from './p3.jpg';
 import p4 from './p4.jpeg';
+import p5 from './p5.jpg';
+import p6 from './p6.jpg';
+import p7 from './p7.jpeg';
+import p8 from './p8.jpeg';
+import p9 from './p9.jpg';
+import p10 from './p10.png';
 
-export default [p0, p1, p2, p3, p4];
+export default [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10];
